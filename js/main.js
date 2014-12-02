815f229e3aa9234231ec2a3c5328452976bdde32
Brigitta Glanz <glanzb@icloud.com>
2014-12-02 13:36:30 -0800

    wireframe

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/* ------------------------
+Image Gallery
+-------------------------*/
+
+//click on thumbnail image
+$('li img').on('click',function(){
+    var src = $(this).attr('src');
+    var img = '<img src="' + src + '" class="img-responsive"/>';
+    // index method traverses from thumbnail to parent li item and stores its index
+    // first array index is 0
+    var index = $(this).parent('li').index();
+
+    var html = '';
+    html += img;                
+    html += '<div style="height:25px;clear:both;display:block;">';
+    html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
+    html += '<a class="controls previous" href="' + (index) + '">&laquo; prev</a>';
+    html += '</div>';
+
+    $('#myModal').modal();
+    $('#myModal').on('shown.bs.modal', function(){
+    //hide or show the right links when first or last image is clicked
+      $('#myModal .modal-body').html(html);
+      $('a.controls').trigger('click');
+    })
+    $('#myModal').on('hidden.bs.modal', function(){
+      $('#myModal .modal-body').html('');
+    });
+  });
+
+
+//set up click handler for previous and next buttons
+$(document).on('click', 'a.controls', function(){
+  var index = $(this).attr('href');
+  var src = $('ul.imageGallery li:nth-child('+ index + ') img').attr('src');
+  $('.modal-body img').attr('src', src);
+
+  var newPrevIndex = parseInt(index) - 1;
+  var newNextIndex = parseInt(newPrevIndex) + 2;
+
+  if($(this).hasClass('previous')) {
+    $(this).attr('href', newPrevIndex);
+    $('a.next').attr('href', newNextIndex);
+  } else {
+    $(this).attr('href', newNextIndex);
+    $('a.previous').attr('href', newPrevIndex);
+  }
+
+  // hide "next" link on last image and "prev" on first
+  var total = $('ul.imageGallery li').length + 1;
+  // hide next button
+  if (total === newNextIndex) {
+    $('a.next').hide();
+  } else {
+    $('a.next').show()
+  }
+  // hide previous button
+  if (newPrevIndex === 0) {
+    $('a.previous').hide();
+  } else {
+    $('a.previous').show()
+  }
+    return false;
+});
